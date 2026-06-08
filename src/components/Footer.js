00f9a340eacc5fad4a709bf2d1ac7236da5587ab
LucasPlebani <lucas.plebani@outlook.fr>
2026-06-08 13:48:24 +0200

    add change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ export default function Footer() {
 
   return (
     <MotionCard as="footer" className="footer" delay={0.35}>
-      <div>© 2026 Gigi — codée avec <img src={heart.src} alt="heart" className="heart-img-footer" /> et beaucoup de <img src={latte.src} alt="Latte" className="coffee-img-footer" />.</div>
+      <div>© 2026 Lucas — codée avec <img src={heart.src} alt="heart" className="heart-img-footer" /> et beaucoup de <img src={latte.src} alt="Latte" className="coffee-img-footer" />.</div>
       <div className="footer-links">
         <a href="#projects" onClick={(e) => { e.preventDefault(); scrollTo("projects"); }}>Projets</a>
         <a href="#contact" onClick={(e) => { e.preventDefault(); scrollTo("contact"); }}>Contact</a>
